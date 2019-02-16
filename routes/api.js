d2877a395440fba070df8454d725bc391f678ffe
Kevin K <kevinkim75@gmail.com>
2019-02-16 15:57:57 -0500

    Limited 1 like per IP address

--- routes/api.js
+++ routes/api.js
@@ -16,7 +16,12 @@ var axios = require('axios');
 const CONNECTION_STRING = process.env.DB;
 
 module.exports = function (app) {
-  MongoClient.connect(CONNECTION_STRING, function(err, db) {
+  MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true }, function(err, client) {
+    if (err) {
+      console.log(err);
+    }
+
+    var db = client.db(process.env.DATABASE);
     var collection = db.collection('stock_likes');
     app.route('/api/stock-prices')
       .get(function (req, res) {
@@ -26,19 +31,26 @@ module.exports = function (app) {
                .send('Missing stock in request');
          }
 
+         var isLiked = req.query.like === 'true';
+         var ip = (req.get('x-forwarded-for') || '').split(',')[0] ||
+           req.connection.remoteAddress || req.socket.remoteAddress ||
+           req.connection.socket.remoteAddress;
          if (typeof req.query.stock === 'string') {
            var stock = req.query.stock.toUpperCase();
-           var isLiked = req.query.like === 'true';
            axios.get('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=' + stock + '&apikey=' + process.env.ALPHPA_VANTAGE_KEY)
              .then(function(response) {
-               if (Object.keys(response.data['Global Quote']).length === 0) {
+               if (Object.keys(response.data['Global Quote'] || {}).length === 0) {
                  return res.status(404)
                    .type('text')
                      .send('Stock ' + stock + ' not found');
                }
 
                var price = parseFloat(response.data['Global Quote']['05. price']).toFixed(2);
-               collection.findOne({ stock: stock }, function(err, doc) {
+               collection.findOne({ stock: stock }, {
+                 projection: {
+                   likes: 1,
+                 },
+               }, function(err, doc) {
                  if (err) {
                    console.log(err);
                  }
@@ -47,6 +59,7 @@ module.exports = function (app) {
                    collection.insertOne({
                      stock: stock,
                      likes: isLiked ? 1 : 0,
+                     ips: [ip],
                    }, function(err, doc) {
                      if (err) {
                        console.log(err);
@@ -55,18 +68,31 @@ module.exports = function (app) {
                      res.json({
                        stockData: {
                          stock: stock,
-                         price: price,
+                          price: price,
                          likes: isLiked ? 1 : 0,
                        },
                      });
                    });
                  } else {
                    if (isLiked) {
-                     collection.findOneAndUpdate({ stock: stock, }, {
+                     collection.findOneAndUpdate({
+                       stock: stock,
+                       ips: {
+                         $ne: ip,
+                       },
+                     }, {
                        $inc: {
                          likes: 1,
                        },
-                     }, { returnOriginal: false, }, function(err, result) {
+                       $push: {
+                         ips: ip,
+                       },
+                     }, {
+                       projection: {
+                         likes: 1,
+                       },
+                       returnOriginal: false,
+                     }, function(err, result) {
                        if (err) {
                          console.log(err);
                        }
@@ -75,7 +101,7 @@ module.exports = function (app) {
                          stockData: {
                            stock: stock,
                            price: price,
-                           likes: result.value.likes,
+                           likes: result.value ? result.value.likes : doc.likes,
                          },
                        });
                      });
@@ -102,7 +128,7 @@ module.exports = function (app) {
       res.status(404)
         .type('text')
         .send('Not Found');
-});
+    });
   });
 };
 
