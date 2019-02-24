c570c9db892e5febd2b61d800e1a43ed0b31ceb9
Kevin K <kevinkim75@gmail.com>
2019-02-23 22:11:46 -0500

    Switched callbacks to promises.

--- routes/api.js
+++ routes/api.js
@@ -25,103 +25,109 @@ module.exports = function (app) {
     var collection = db.collection('stock_likes');
     app.route('/api/stock-prices')
       .get(function (req, res) {
-         if (!req.query.stock) {
-           return res.status(400)
-             .type('text')
-               .send('Missing stock in request');
-         }
+        if (!req.query.stock) {
+          return res.status(400)
+            .type('text')
+              .send('Missing stock in request');
+        }
 
-         var isLiked = req.query.like === 'true';
-         var ip = (req.get('x-forwarded-for') || '').split(',')[0] ||
-           req.connection.remoteAddress || req.socket.remoteAddress ||
-           req.connection.socket.remoteAddress;
-         if (typeof req.query.stock === 'string') {
-           var stock = req.query.stock.toUpperCase();
-           axios.get('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=' + stock + '&apikey=' + process.env.ALPHPA_VANTAGE_KEY)
-             .then(function(response) {
-               if (Object.keys(response.data['Global Quote'] || {}).length === 0) {
-                 return res.status(404)
-                   .type('text')
-                     .send('Stock ' + stock + ' not found');
-               }
-
-               var price = parseFloat(response.data['Global Quote']['05. price']).toFixed(2);
-               collection.findOne({ stock: stock }, {
-                 projection: {
-                   likes: 1,
-                 },
-               }, function(err, doc) {
-                 if (err) {
-                   console.log(err);
-                 }
-
-                 if (doc === null) {
-                   collection.insertOne({
-                     stock: stock,
-                     likes: isLiked ? 1 : 0,
-                     ips: [ip],
-                   }, function(err, doc) {
-                     if (err) {
-                       console.log(err);
-                     }
-
-                     res.json({
-                       stockData: {
-                         stock: stock,
+        var isLiked = req.query.like === 'true';
+        var ip = (req.get('x-forwarded-for') || '').split(',')[0] ||
+          req.connection.remoteAddress || req.socket.remoteAddress ||
+          req.connection.socket.remoteAddress;
+        if (typeof req.query.stock === 'string') {
+          var stock = req.query.stock.toUpperCase();
+          axios.get('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=' + stock + '&apikey=' + process.env.ALPHPA_VANTAGE_KEY)
+            .catch(function(err) {
+              console.log(err);
+            })
+            .then(function(response) {
+              if (Object.keys(response.data['Global Quote'] || {}).length === 0) {
+                return res.status(404)
+                  .type('text')
+                    .send('Stock ' + stock + ' not found');
+              }
+               
+              var price = parseFloat(response.data['Global Quote']['05. price']).toFixed(2);
+              collection.findOne({ stock: stock }, {
+                projection: {
+                  likes: 1,
+                },
+              })
+                .catch(function(err) {
+                  if (err) {
+                    console.log(err);
+                  }
+                })
+                .then(function(doc) {
+                  if (doc === null) {
+                    collection.insertOne({
+                      stock: stock,
+                      likes: isLiked ? 1 : 0,
+                      ips: [ip],
+                    })
+                      .catch(function(err) {
+                        if (err) {
+                          console.log(err);
+                        }
+                      })
+                      .then(function(doc) {
+                        res.json({
+                          stockData: {
+                            stock: stock,
+                            price: price,
+                            likes: isLiked ? 1 : 0,
+                          },
+                        });
+                      });
+                  } else {
+                    if (isLiked) {
+                      collection.findOneAndUpdate({
+                        stock: stock,
+                        ips: {
+                          $ne: ip,
+                        },
+                      }, {
+                        $inc: {
+                          likes: 1,
+                        },
+                        $push: {
+                          ips: ip,
+                        },
+                      }, {
+                        projection: {
+                          likes: 1,
+                        },
+                        returnOriginal: false,
+                      })
+                        .catch(function(err) {
+                          if (err) {
+                            console.log(err);
+                          }
+                        })
+                        .then(function(result) {
+                          res.json({
+                            stockData: {
+                              stock: stock,
+                              price: price,
+                              likes: result.value ? result.value.likes : doc.likes,
+                            },
+                          });
+                        });
+                    } else {
+                      res.json({
+                        stockData: {
+                          stock: stock,
                           price: price,
-                         likes: isLiked ? 1 : 0,
-                       },
-                     });
-                   });
-                 } else {
-                   if (isLiked) {
-                     collection.findOneAndUpdate({
-                       stock: stock,
-                       ips: {
-                         $ne: ip,
-                       },
-                     }, {
-                       $inc: {
-                         likes: 1,
-                       },
-                       $push: {
-                         ips: ip,
-                       },
-                     }, {
-                       projection: {
-                         likes: 1,
-                       },
-                       returnOriginal: false,
-                     }, function(err, result) {
-                       if (err) {
-                         console.log(err);
-                       }
-
-                       res.json({
-                         stockData: {
-                           stock: stock,
-                           price: price,
-                           likes: result.value ? result.value.likes : doc.likes,
-                         },
-                       });
-                     });
-                   } else {
-                     res.json({
-                       stockData: {
-                         stock: stock,
-                         price: price,
-                         likes: doc.likes,
-                       },
-                     });
-                   }
-                 }
-               });
-             })
-               .catch(function(err) {
-                 console.log(err);
-               });
-          }
-        });
+                          likes: doc.likes,
+                        },
+                      });
+                    }
+                  }
+                });
+            });
+        }
+      });
     
     //404 Not Found Middleware
     app.use(function(req, res, next) {
